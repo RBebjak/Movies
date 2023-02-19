fad20ca10925272059743e9d6f594430b468e253
RBebjak <richard.bebjak01@gmail.com>
2023-02-19 23:41:16 +0100

    Implement comments

--- Simpledatabase.py
+++ Simpledatabase.py
@@ -1,18 +1,36 @@
 class SimpleDatabase():
+    """Simple class of movie database, based on basic python dictionaries
+    """
     def __init__(self):
         self.database = {}
         self.id_list = []
 
     def get_new_id(self):
+        """For 'POST' command to give new movie his id
+         when it`s uploaded to database
+
+        Returns:
+            int: New id for new movie
+        """
         if not self.id_list:
             return 1
 
         return self.id_list[-1] + 1
 
     def log_new_id(self, id):
+        """Adds id to list that new id for new movie could be created
+
+        Args:
+            id (int): id of last added movie
+        """
         self.id_list.append(id)
 
     def list(self):
+        """List of all movies
+
+        Returns:
+            List: List of all movies in database
+        """
         movie_list = []
 
         for entry in self.database.values():
@@ -21,16 +39,34 @@ class SimpleDatabase():
         return movie_list
 
     def get_by_id(self, id):
+        """Find movie by his id
+
+        Args:
+            id (int): id of wanted movie
+
+        Returns:
+            dict: wanted movie
+        """
         return self.database.get(id, None)
 
     def insert(self, title, release_year, description=None):
+        """Insert new movie to table
+
+        Args:
+            title (str): Title of movie
+            release_year (int): release year of movie
+            description (str, optional): description of movie. Defaults to None.
+
+        Returns:
+            dict: new movie
+        """
         new_id = self.get_new_id()
         movie_entry = {
             'id': new_id,
             'title': title,
             'release_year': release_year
         }
-
+#If description is given add it into the table
         if description:
             movie_entry['description'] = description
         self.database[new_id] = movie_entry
@@ -39,6 +75,15 @@ class SimpleDatabase():
         return movie_entry
 
     def update(self, id, changes):
+        """Upload changes into the table to specific movie
+
+        Args:
+            id (int): id of movie which is wanted to be changed
+            changes (dict): List of changes wanted to be done
+
+        Returns:
+            dict: Upload movie with changes
+        """
         entry = self.database.get(id, None)
 
         if not entry:
--- movies.py
+++ movies.py
@@ -14,14 +14,25 @@ NOT_FOUND = ('Not Found', 404)
 
 @app.route('/movies', methods=['GET'])
 def get_movies():
+    """Loading whole table from database
+
+    Returns:
+        str: str reprezentation in json format of all movies
+    """
     movies = db.list()
     return jsonify(movies)
 
 
 @app.route('/movies/<int:movie_id>', methods=['GET'])
 def get_movie(movie_id):
+    """Load row from table with specific id
+
+    Args:
+        movie_id (int): id of movie to be returned
+    """
     movie = db.get_by_id(movie_id)
 
+#if id is not in table return ERROR
     if not movie:
         return Response(
             NOT_FOUND[0],
@@ -33,14 +44,19 @@ def get_movie(movie_id):
 
 @app.route('/movies', methods=['POST'])
 def upload_movie():
+    """Upload new movie in the table
+    """
     movie = request.json
 
+#if request does not have specific arguments return ERROR
     if not __check_post__(movie.keys()):
         return Response(
             BAD_REQUEST[0],
             status=BAD_REQUEST[1]
         )
 
+#with correct arguments upload movie in the table
+#with or without description, depends on command
     if movie.get('description', ""):
         new_movie = db.insert(
             movie.get('title'),
@@ -58,10 +74,16 @@ def upload_movie():
 
 @app.route('/movies/<int:movie_id>', methods=['PUT'])
 def update_movie(movie_id):
+    """Upload changes to the table
+
+    Args:
+        movie_id (int): id of the movie to be changed
+    """
     movie = request.json
 
     changed_entry = db.update(movie_id, movie)
 
+#if movie id not found in the table return ERROR
     if not changed_entry:
         return Response(
             NOT_FOUND[0],
@@ -72,6 +94,15 @@ def update_movie(movie_id):
 
 
 def __check_post__(post_keys):
+    """Checking if 'POST' command got right arguments
+
+    Args:
+        post_keys (List): List of keys that are nessessary
+        for upload new movie
+
+    Returns:
+        Bool: If the list of keys is correct
+    """
     required_keys = ('title', 'release_year')
 
     for key in required_keys:
